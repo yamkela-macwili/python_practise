69f58835a759da999836a61bf4d42800f0e915f2
yamaccpt025 <yamaccpt025@student.wethinkcode.co.za>
2025-10-17 14:41:12 +0200

    Cookies

--- first_flask_app/app.py
+++ first_flask_app/app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, make_response
 
-app = Flask(__name__)
+app = Flask(__name__) 
+@app.route('/') 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+def index(): 
+    return render_template('index.html') 
 
-if __name__ == '__main__':
+@app.route('/setcookie', methods = ['POST', 'GET']) 
+def setcookie(): 
+    if request.method == 'POST': 
+        user = request.form['nm'] 
+        resp = make_response(render_template('cookie.html')) 
+        resp.set_cookie('userID', user) 
+        return resp 
+
+@app.route('/getcookie') 
+def getcookie(): 
+    name = request.cookies.get('userID') 
+    return '<h1>welcome '+name+'</h1>'
+if __name__ == "__main__":
     app.run(debug=True)
